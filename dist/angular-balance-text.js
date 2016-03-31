b2f04208bf35237a0c998169f0d36a61cbe04942
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-03-31 09:58:22 +0200

    bump version

--- dist/angular-balance-text.js
+++ dist/angular-balance-text.js
@@ -1,3 +1,10 @@
+/**
+    @name: angular-balance-text 
+    @version: 1.0.0 (31-03-2016) 
+    @author: Jonathan Hornung <jonathan.hornung@gmail.com> 
+    @url: https://github.com/JohnnyTheTank/angular-balance-text#readme 
+    @license: MIT
+*/
 angular.module('jtt_balance_text', [])
     .directive('balanceText', function () {
         return {
